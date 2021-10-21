b277872947b7cba8a793670bb5216f2cb9729c5b
Josh Nice <joshliamnice@gmail.com>
2021-10-21 19:18:50 +0400

    Added parsed csv type

--- src/types/csv.types.ts
+++ src/types/csv.types.ts
@@ -1,8 +1,19 @@
+import { Point, Polygon } from "geojson";
+
 export interface RawCSVHeaders {
     id: string;
     name: string;
     bounds: string;
     geom: string;
+    address: string;
+    siteid: string;
+}
+
+export interface ParsedCSVHeaders {
+    id: string;
+    name: string;
+    bounds: Polygon;
+    geom: Point;
     address: string;
     siteid: string;
 }
