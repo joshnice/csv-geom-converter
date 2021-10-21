aa09f2d554e3d930152bd4a9ac11c99e5782cb05
Josh Nice <joshliamnice@gmail.com>
2021-10-21 20:08:34 +0400

    Added writing stage

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,24 @@
 import { createReadStream } from "fs";
 import { parse } from "fast-csv"; 
 import { resolve as pathResolve } from "path";
-import { RawCSVHeaders } from "./types/csv.types";
+import { StringCSVHeaders } from "./types/csv.types";
+import { parseCSVValues } from "./helpers/csv-value-parser";
+import { mutateGeojsonValues } from "./helpers/geojson-to-string-convert";
+import { createObjectCsvWriter } from "csv-writer";
+
 const clear = require('clear');
 
 async function main() {
     clearConsole();
-    const output = await csvImport();
-    console.log("output", output[1]);
+    const importedData = await csvImport();
+    const parsedImportedData = parseCSVValues(importedData);
+    const mutatedGeomValues = mutateGeojsonValues(parsedImportedData);
+    await writeCSV(mutatedGeomValues);
 }
 
-function csvImport(): Promise<RawCSVHeaders[]> {
+function csvImport(): Promise<StringCSVHeaders[]> {
 
-    const csvValues: RawCSVHeaders[] = [];
+    const csvValues: StringCSVHeaders[] = [];
 
     return new Promise((resolve, reject) => {
         createReadStream(pathResolve(__dirname, '../assets', 'parse.csv'), 'utf8')
@@ -28,6 +34,32 @@ function csvImport(): Promise<RawCSVHeaders[]> {
     });
     
 }
+
+function writeCSV(csvValues: StringCSVHeaders[]) {
+    const csvWriter = createObjectCsvWriter({ 
+        path: pathResolve(__dirname, '../assets', 'parsed.csv'),
+        header: [
+            {id: "id", title: "id"},
+            {id: "name", title: "name"},
+            {id: "geom", title: "geom"},
+            {id: "bounds", title: "bounds"},
+            {id: "address", title: "address"},
+            {id: "siteid", title: "siteid"},
+        ],
+    });
+
+    return new Promise<void>((resolve, reject) => {
+        csvWriter.writeRecords(csvValues)
+        .then(() => {
+            console.log('Export CSV');
+            resolve();
+        })
+        .catch(() => {
+            reject();
+        })
+    });
+ };
+
 
 function clearConsole() {
     clear();
