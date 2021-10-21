979e2c15022cfd4b856ef98f6de8f66c188a3c67
Josh Nice <joshliamnice@gmail.com>
2021-10-21 19:56:37 +0400

    Added in covert for geojson to string

---
+++ src/helpers/geojson-to-string-convert.ts
@@ -0,0 +1,27 @@
+import { Point, Polygon } from "geojson";
+import { ParsedCSVHeaders, StringCSVHeaders } from "../types/csv.types";
+
+export function mutateGeojsonValues(values: ParsedCSVHeaders[]): StringCSVHeaders[] {
+    return values.map((value) => ({...mutateGeojsonValue(value)}));
+}
+
+function mutateGeojsonValue(value: ParsedCSVHeaders): StringCSVHeaders {
+    return {
+        id: value.id,
+        name: value.name,
+        bounds: convertBounds(value.bounds),
+        geom: convertGeom(value.geom),
+        address: value.address,
+        siteid: value.siteid,
+    }
+}
+
+function convertBounds(bounds: Polygon): string {
+    const coordinates = bounds.coordinates[0];
+    const coordinates_pairs = coordinates.map(coordinate => coordinate.join(" "));
+    return `POLYGON (( ${coordinates_pairs.join(",")} ))`;
+}
+
+function convertGeom(geom: Point): string {
+    return `POINT ( ${geom.coordinates.join(" ")} )`
+}
