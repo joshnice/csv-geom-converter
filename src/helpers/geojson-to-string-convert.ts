75d0e3204af208cffb5d11f5dc773c0913ea94d0
Josh Nice <joshliamnice@gmail.com>
2021-10-26 13:13:33 +0400

    Added import from JSON file

--- src/helpers/geojson-to-string-convert.ts
+++ src/helpers/geojson-to-string-convert.ts
@@ -1,23 +1,43 @@
-import { Point, Polygon } from "geojson";
+import { MultiPolygon, Point, Polygon } from "geojson";
 import { ParsedCSVHeaders, StringCSVHeaders } from "../types/csv.types";
 
-export function mutateGeojsonValues(values: ParsedCSVHeaders[]): StringCSVHeaders[] {
-    return values.map((value) => ({...mutateGeojsonValue(value)}));
+interface LocationJSONData {
+    type: string,
+    properties: {
+        featureId: number,
+        Venue: string,
+        deleted: number,
+        layerid: number,
+        geomtype: number,
+        level_id: number,
+    },
+    geometry: MultiPolygon,
 }
 
-function mutateGeojsonValue(value: ParsedCSVHeaders): StringCSVHeaders {
+export function mutateGeojsonValues(values: ParsedCSVHeaders[]): StringCSVHeaders[] {
+    const data = require("../../assets/area.json");
+    const areaValues: LocationJSONData[] = data.features; 
+    return values.map((value) => ({...mutateGeojsonValue(value, areaValues)}));
+}
+
+function mutateGeojsonValue(value: ParsedCSVHeaders, areaValues: LocationJSONData[]): StringCSVHeaders {
     return {
         name: value.name,
-        bounds: value.bounds != null ? convertBounds(value.bounds) : null,
+        bounds: value.bounds != null ? convertBounds(areaValues, value.name) : null,
         geom: convertGeom(value.geom),
         address: value.address,
         siteid: value.siteid,
     }
 }
 
-function convertBounds(bounds: Polygon): string {
-    const coordinates = bounds.coordinates[0];
-    const coordinates_pairs = coordinates.map(coordinate => coordinate.join(" "));
+function convertBounds(areaValues: LocationJSONData[], venueName: string): string | null {
+    const venue = areaValues.find(val => val.properties.Venue === venueName);
+
+    if (venue == null) {
+        return null;
+    }
+
+    const coordinates_pairs = venue.geometry.coordinates[0][0].map(coordinate => coordinate.join(" "));
     return `SRID= 4326; POLYGON (( ${coordinates_pairs.join(",")} ))`;
 }
 
